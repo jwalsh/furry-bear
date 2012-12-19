4f169f1aafcd13dc44d22583a0f33acf76520ec9
Jason Walsh <jwalsh@bluekai.com>
2012-12-18 16:11:31 -0800

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to furry-bear
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/furry_bear/core_test.clj
@@ -0,0 +1,7 @@
+(ns furry-bear.core-test
+  (:use clojure.test
+        furry-bear.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
